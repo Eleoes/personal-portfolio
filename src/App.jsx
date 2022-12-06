6357030b860409b86a0ca0f20077c57c5ebe68b3
eleoes <leo.escaroz@gmail.com>
2022-12-05 18:59:43 -0800

    wip: Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
+import Header from './components/header/Header';
 
-function App() {
-
+const App = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-      <i class='bx bx-user'></i>
-      <i class='bx bxs-user'></i>
-      <i class='bx bxl-html5'></i>
-    </div>
+    <>
+    <Header />
+    </>
   )
 }
 
-export default App
+export default App;
