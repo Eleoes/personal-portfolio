ec34772855087b92fe4e75197d4e0771eeea8e9f
eleoes <leo.escaroz@gmail.com>
2022-12-10 15:22:03 -0800

    wip: Home component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.linkedin.com/in/leonardo-escaroz-b1b47113b/" className="home__social-icon" target="_blank">
+                <i class="uil uil-linkedin-alt"></i>
+            </a>
+
+            <a href="https://github.com/Eleoes" className="home__social-icon" target="_blank">
+                <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social;
