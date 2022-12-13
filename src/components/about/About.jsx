6a816e1de0797eee0cc793d2985bc417417bf90c
eleoes <leo.escaroz@gmail.com>
2022-12-12 20:37:47 -0800

    wip: About, Resume button

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import "./about.css";
+import Portrait from "../../assets/Portrait.jpg";
+import Resume from "../../assets/Leo Escaroz_Resume.pdf";
+import Info from './Info';
 
 const About = () => {
     return (
-        <div>About</div>
+        <section className="about section" id="about">
+            <h2 className="section__title">About Me</h2>
+            <span className="section__subtitle">My introduction</span>
+
+            <div className="about__container container grid">
+                <img src={Portrait} alt="My Portrait" className="about__img" />
+
+                <div className="about__data">
+                    <Info />
+
+                    <p className="about__description">
+                        Web developer, I create web pages ...
+                    </p>
+
+                    <a download="" href={Resume} className="button button--flex">Resume</a>
+                </div>
+            </div>
+        </section>
     )
 }
 
