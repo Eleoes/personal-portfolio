d70b40088b0a0d2f0f423e455abf2ca3874f1a74
eleoes <leo.escaroz@gmail.com>
2022-12-15 19:22:32 -0800

    wip: Portfolio, Projects Data

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <About />
       <Skills />
       <Services />
+      <Portfolio />
     </main>
     </>
   )
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./portfolio.css";
+import Projects from "./Projects";
+
+const Portfolio = () => {
+    return (
+        <section className="portfolio section" id="portfolio">
+            <h2 className="section__title">Portfolio</h2>
+            <span className="section__subtitle">Most recent works</span>
+
+            <Projects />
+        </section>
+    )
+}
+
+export default Portfolio;
